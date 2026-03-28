9b74385b8fc09eb1a180eb14232c3f0c2127fb6a
supun-pregeeth <ruchiraniasanka@gmail.com>
2026-03-28 23:24:22 +0530

    implement the room repo

--- hotel-management/src/main/java/com/hotel/hotel_management/room/repository/RoomRepository.java
+++ hotel-management/src/main/java/com/hotel/hotel_management/room/repository/RoomRepository.java
@@ -1,4 +1,29 @@
 package com.hotel.hotel_management.room.repository;
 
-public class RoomRepository {
+import com.hotel.hotel_management.room.entity.Room;
+import com.hotel.hotel_management.room.enums.RoomStatus;
+import com.hotel.hotel_management.room.enums.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    //available all rooms
+    List<Room> findByStatus(RoomStatus status);
+
+    //find all rooms by type
+    List<Room> findByType(RoomType type);
+
+    //Combined filter (powerful)
+    List<Room> findByTypeAndStatus(RoomType type, RoomStatus status);
+
+    //find by room number
+    Optional<Room> findByRoomNumber(String room);
+
+    //Check if room exists (useful for validation)
+    boolean existsByRoomNumber(String roomNumber);
 }
